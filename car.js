62ea870f1ab0f32c8a96dee44e091ae9cb3b8fbc
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-06-18 00:42:38 +0100

    Adds maxspeed and friction

--- car.js
+++ car.js
@@ -6,16 +6,36 @@ class Car {
     this.height = height;
 
     this.speed = 0;
-    this.accelegaration = 0.2;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
     this.controls = new Controls();
   }
 
   update() {
     if (this.controls.forward) {
-      this.speed += this.accelegaration;
+      this.speed += this.acceleration;
     }
+
     if (this.controls.reverse) {
-      this.speed -= this.accelegaration;
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+
+    if (this.speed < 0) {
+      this.speed += this.friction;
     }
     this.y -= this.speed;
   }
