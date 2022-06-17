5124f33efcc4cbc93c748c48100bac7bada988f6
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-06-18 00:31:33 +0100

    Add speed and acceleration

--- car.js
+++ car.js
@@ -1,21 +1,33 @@
 class Car {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.controls = new Controls();
-    }
+    this.speed = 0;
+    this.accelegaration = 0.2;
+    this.controls = new Controls();
+  }
 
-    draw(ctx) {
-        ctx.beginPath();
-        ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+  update() {
+    if (this.controls.forward) {
+      this.speed += this.accelegaration;
     }
+    if (this.controls.reverse) {
+      this.speed -= this.accelegaration;
+    }
+    this.y -= this.speed;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
 }
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 // draw car
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
+
+// animate
+animate();
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
