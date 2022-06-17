d6bc872b16c9a82e7fc275d131c25ade0198ec5c
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-06-18 00:56:40 +0100

    Improves car left and right movement

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
+    this.angle = 0;
 
     this.controls = new Controls();
   }
@@ -37,17 +38,28 @@ class Car {
     if (this.speed < 0) {
       this.speed += this.friction;
     }
-    this.y -= this.speed;
+
+    if (this.controls.left) {
+      this.angle += 0.03;
+    }
+
+    if (this.controls.right) {
+      this.angle -= 0.03;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
