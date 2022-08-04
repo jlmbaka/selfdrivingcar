76ae67717631fe859c59dcd36981b90a78287d64
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-08-04 11:39:23 +0100

    Displaying the output layer

--- visualiser.js
+++ visualiser.js
@@ -12,16 +12,29 @@ class Visualiser {
   static drawLevel(ctx, level, left, top, width, height) {
     const right = left + width;
     const bottom = top + height;
-
+    const { inputs, outputs } = level;
     const nodeRadius = 18;
-    for (let i = 0; i < level.inputs.length; i++) {
+
+    for (let i = 0; i < inputs.length; i++) {
       const x = lerp(
         left,
         right,
-        level.inputs.length == 1 ? 0.5 : i / (level.inputs.length - 1)
+        inputs.length == 1 ? 0.5 : i / (inputs.length - 1)
       );
       ctx.beginPath();
       ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2);
+      ctx.fillStyle = "white";
+      ctx.fill();
+    }
+
+    for (let i = 0; i < outputs.length; i++) {
+      const x = lerp(
+        left,
+        right,
+        outputs.length == 1 ? 0.5 : i / (outputs.length - 1)
+      );
+      ctx.beginPath();
+      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2);
       ctx.fillStyle = "white";
       ctx.fill();
     }
