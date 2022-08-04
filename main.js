730bc1330b989e6f9fcf10cb32a95e7f3bc993b6
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-08-04 11:36:43 +0100

    Drawing circles on the network canvas

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
-const ctx = canvas.getContext("2d");
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 
 // draw road
 const margin = 0.9;
-const road = new Road(canvas.width / 2, canvas.width * margin);
+const road = new Road(carCanvas.width / 2, carCanvas.width * margin);
 
 // draw car
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
 // the traffic
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
+
 // animate
 animate();
 
 function animate() {
   for (let i = 0; i < traffic.length; i++) traffic[i].update(road.borders, []);
   car.update(road.borders, traffic);
-  canvas.height = window.innerHeight;
 
-  ctx.save();
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+
+  carCtx.save();
 
   keepCarCentredAt(car);
 
-  road.draw(ctx);
-  for (let i = 0; i < traffic.length; i++) traffic[i].draw(ctx, "red");
-  car.draw(ctx, "blue");
+  road.draw(carCtx);
+  for (let i = 0; i < traffic.length; i++) traffic[i].draw(carCtx, "red");
+  car.draw(carCtx, "blue");
 
-  ctx.restore();
+  carCtx.restore();
 
+  Visualiser.drawNetwork(networkCtx, car.brain);
   requestAnimationFrame(animate);
 }
 
 function keepCarCentredAt(car, percent = 0.7) {
-  ctx.translate(0, -car.y + canvas.height * 0.7); // keep the car at 70%
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7); // keep the car at 70%
 }
