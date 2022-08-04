ac577e9c3dea7d312c55620b644e7b97fc078478
Jean-Louis <jeanlouis.mbaka@gmail.com>
2022-08-04 07:30:38 +0100

    Hidden layers

---
+++ network.js
@@ -0,0 +1,57 @@
+class NeuralNetwork {
+  constructor(neuronCounts) {
+    this.levels = [];
+    for (let i = 0; i < neuronCounts.length; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForward(givenInputs, network) {
+    let outputs = Level.feedForward(givenInputs, network.levels[0]);
+    for (let i = 0; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.levels[i]);
+    }
+    return outputs;
+  }
+}
+
+class Level {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount);
+    this.outputs = new Array(outputCount);
+    this.biases = new Array(outputCount);
+
+    this.weights = [];
+    for (let i = 0; i < inputCounts; i++) {
+      this.weights[i] = new Array(outputCount);
+    }
+    Level.#randomize(this);
+  }
+
+  static #randomize(level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1;
+      }
+    }
+
+    for (let i = 0; i < level.biases.length; i++) {
+      level.biases[i] = Math.random() * 2 - 1;
+    }
+  }
+
+  static feedForward(givenInputs, level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i];
+    }
+    for (let i = 0; i < level.outputs.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < level.input.length; j++) {
+        sum += level.input[j] * level.weights[j][i];
+      }
+      level.inputs[i] = givenInputs[i];
+    }
+    level.outputs[i] = sum + level.biases[i] > 0 ? 1 : 0;
+    return level.outputs;
+  }
+}
